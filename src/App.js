bafe55dd8ea04f5ef8ed2dd83cdc99670b349c49
hupeijie <hupeijie@brilliance.com.cn>
2021-04-18 19:24:54 +0800

    第一次提交   初版

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+import Home from './components/Home';
+import News from './components/News';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      你好 react 组件
+      <Home/>
+      <News />
     </div>
   );
 }
